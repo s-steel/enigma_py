62d7369b59aec4e8b605517810bbfdf1ad94afee
Sean Steel <seanrsteel@gmail.com>
2021-03-03 08:30:32 -0700

    Create shift_letter method with tests

--- enigma.py
+++ enigma.py
@@ -3,7 +3,8 @@ import random
 from datetime import date
 
 class Enigma: 
-  letters = list(string.ascii_lowercase).append(' ')
+  letters = list(string.ascii_lowercase)
+  letters.append(' ')
 
   def generate_key(self):
     digits = random.sample(range(0, 10), 5)
@@ -14,3 +15,21 @@ class Enigma:
 
   def get_offset(self, date):
     return str(int(date) * int(date))[-4:]
+
+  def get_shift(self, key, date):
+    offset = self.get_offset(date)
+    shift = [
+      int(key[:2]) + int(offset[:1]),
+      int(key[1:3]) + int(offset[1:2]),
+      int(key[2:4]) + int(offset[2:3]),
+      int(key[3:5]) + int(offset[3:4])
+    ]
+    return shift
+
+  def shift_letter(self, letter, shift):
+    letter_index = self.letters.index(letter)
+    if letter_index + shift > 27:
+      shift = (letter_index + shift) - 27
+      return self.letters[shift]
+    else:
+      return self.letters[letter_index + shift]
--- test_enigma.py
+++ test_enigma.py
@@ -24,6 +24,27 @@ def test_get_offset():
 
   assert expected == offset
 
+def test_get_shift():
+  enigma = Enigma()
+  key = '02715'
+  date = '040895'
+  expected = [3, 27, 73, 20]
+  shift = enigma.get_shift(key, date)
+
+  assert expected == shift
+
+def test_shift_letter():
+  enigma = Enigma()
+  char_1 = 'a'
+  shift_1 = 3
+  char_2 = 'q'
+  shift_2 = 15
+  char_3 = 'y'
+  shift_3 = 2
+
+  assert enigma.shift_letter(char_1, shift_1) == 'd'
+  assert enigma.shift_letter(char_2, shift_2) == 'e'
+  assert enigma.shift_letter(char_3, shift_3) == ' '
 
 # # encrypt a message with a key and date
 # pry(main)> enigma.encrypt("hello world", "02715", "040895")
